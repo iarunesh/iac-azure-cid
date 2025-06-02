2a6be13d263e5dca9ab0442caa19d8f31e0164af
iarunesh <49444925+iarunesh@users.noreply.github.com>
2025-06-02 16:40:53 +0530

    Update ouputs.tf

--- ouputs.tf
+++ ouputs.tf
@@ -1,3 +1,14 @@
-output "webapp_url" {
-  value = azurerm_linux_web_app.this.default_hostname
+output "web_app_url" {
+  description = "The default URL of the deployed Web App"
+  value       = "https://${azurerm_linux_web_app.app.default_hostname}"
+}
+
+output "app_insights_key" {
+  description = "Instrumentation key for Application Insights"
+  value       = azurerm_application_insights.app_insights.instrumentation_key
+}
+
+output "app_insights_connection_string" {
+  description = "Connection string for Application Insights"
+  value       = azurerm_application_insights.app_insights.connection_string
 }
