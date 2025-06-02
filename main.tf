64331d0428a413ccd60f19cb1598ae5de7817e7b
iarunesh <49444925+iarunesh@users.noreply.github.com>
2025-06-02 16:40:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,44 +1,41 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "this" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_service_plan" "this" {
+resource "azurerm_app_service_plan" "plan" {
   name                = "${var.app_name}-plan"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  os_type             = "Linux"
-  sku_name            = "B1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
 }
 
-resource "azurerm_linux_web_app" "this" {
+resource "azurerm_application_insights" "app_insights" {
+  name                = "${var.app_name}-ai"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+resource "azurerm_linux_web_app" "app" {
   name                = var.app_name
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  service_plan_id     = azurerm_service_plan.this.id
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_app_service_plan.plan.id
 
   site_config {
-    application_stack {
-      node_version = "18-lts"
-    }
+    linux_fx_version = "NODE|20-lts"
   }
 
   app_settings = {
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-  }
-}
-
-resource "azurerm_app_service_source_control" "github_actions" {
-  app_id                  = azurerm_linux_web_app.this.id
-  repo_url                = var.github_repo_url
-  branch                  = var.github_branch
-  use_manual_integration  = false
-  use_mercurial           = false
-  github_action_configuration {
-    generate_workflow_file = true
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE"           = "false"
+    "SCM_DO_BUILD_DURING_DEPLOYMENT"                = "true"
+    "APPINSIGHTS_INSTRUMENTATIONKEY"                = azurerm_application_insights.app_insights.instrumentation_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING"         = azurerm_application_insights.app_insights.connection_string
+    "ApplicationInsightsAgent_EXTENSION_VERSION"    = "~3"
   }
 }
